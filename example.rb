37d8793058530f79ebb91dca47bac87c526a62b8
haseebeqx <haseebeqx@users.noreply.github.com>
2025-10-24 16:44:42 +0530

    added scenarios in the question to the example

--- example.rb
+++ example.rb
@@ -24,10 +24,50 @@ red_widget_offer = BuyOneGetOneForHalfPriceOffer.new(red_widget)
 puts "Creating basket"
 basket = Basket.new(catalogue, delivery_rules, [red_widget_offer])
 
+puts "Empty basket total: $#{basket.total.truncate(2)}"
+
 puts "Adding items"
 basket.add_item("R01", 2)  # 2 Red Widgets (offer applies)
 basket.add_item("G01")     # 1 Green Widget
 basket.add_item("B01")     # 1 Blue Widget
 
 puts "Getting total"
-puts "Total: $#{basket.total.round(2)}"
+puts "Total: $#{basket.total.truncate(2)}"
+
+puts "TEST 1"
+
+basket = Basket.new(catalogue, delivery_rules, [red_widget_offer])
+basket.add_item("B01")
+basket.add_item("G01")
+puts "Total: $#{basket.total.truncate(2)}"
+puts "Expected total: $37.85"
+
+puts "TEST 2"
+
+basket = Basket.new(catalogue, delivery_rules, [red_widget_offer])
+
+basket.add_item("R01")
+basket.add_item("R01")
+puts "Total: $#{basket.total.truncate(2)}"
+puts "Expected total: $54.37"
+
+puts "TEST 3"
+
+basket = Basket.new(catalogue, delivery_rules, [red_widget_offer])
+
+basket.add_item("R01")
+basket.add_item("G01")
+puts "Total: $#{basket.total.truncate(2)}"
+puts "Expected total: $60.85"
+
+puts "TEST 4"
+
+basket = Basket.new(catalogue, delivery_rules, [red_widget_offer])
+
+basket.add_item("B01")
+basket.add_item("B01")
+basket.add_item("R01")
+basket.add_item("R01")
+basket.add_item("R01")
+puts "Total: $#{basket.total.truncate(2)}"
+puts "Expected total: $98.27"
